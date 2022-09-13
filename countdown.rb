16f0be5821c548572485b2420aad22453e4e4f04
Duy Pham <ericpham19@outlook.com>
2022-09-13 10:15:59 +1000

    finished

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,22 @@
 #write your code here
 
 def countdown
+    counter = 10
+    while counter > 0
+        puts "#{counter} SECONDS(S)!"
+        counter -= 1
+        
+    end
+    return "HAPPY NEW YEAR!"
+end
+
+
+def countdown_with_sleep (counter)
+  
+    while counter > 0
+        puts "#{counter} SECONDS(S)!"
+        counter -= 1
+        sleep (1)
+    end
+    return "HAPPY NEW YEAR!"
 end
